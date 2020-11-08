9d6eaef9cfad8628bed186ef5ba36891adaeb08a
Zhanghongchen <zhanghongchen1998@126.com>
2020-11-08 16:28:58 +0800

    view create

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,12 @@
+/*
+ * @Author: zhc
+ * @Date: 2020-11-08 01:29:55
+ * @LastEditTime: 2020-11-08 14:53:17
+ * @Description: 
+ * @LastEditors: zhc
+ */
 import 'package:flutter/material.dart';
+import 'package:swiper_with_thumb/ThumbSwiper.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,38 +19,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Swiper with thumb example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,67 +37,65 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  List<Map> imageList = [
+    {
+      'type': 'network',
+      'path':
+          'https://img1.qunarzz.com/travel/d6/1701/c3/ad1a5ce13c3e2bb5.jpg?ver=1',
+    },
+    {
+      'type': 'network',
+      'path':
+          'https://img1.qunarzz.com/travel/d8/1512/b3/0aa3d7eb52cd80f7.jpg_r_1360x1360x95_3ed3350c.jpg',
+    },
+    {
+      'type': 'network',
+      'path':
+          'https://img1.qunarzz.com/travel/d6/1512/60/9a04f34cb80a2bf7.jpg_r_1360x1360x95_8ee9e53c.jpg',
+    },
+    {
+      'type': 'network',
+      'path':
+          'https://img1.qunarzz.com/travel/d6/1701/c3/ad1a5ce13c3e2bb5.jpg?ver=1',
+    },
+    {
+      'type': 'network',
+      'path':
+          'https://img1.qunarzz.com/travel/d8/1512/b3/0aa3d7eb52cd80f7.jpg_r_1360x1360x95_3ed3350c.jpg',
+    },
+    {
+      'type': 'network',
+      'path':
+          'https://img1.qunarzz.com/travel/d6/1512/60/9a04f34cb80a2bf7.jpg_r_1360x1360x95_8ee9e53c.jpg',
+    },
+  ];
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          width:double.maxFinite,
+          child: ThumbSwiper(
+            imageList: imageList,
+            height: 400,
+            width: 200,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
